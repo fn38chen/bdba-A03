2eb3661c6765fc96aa60c72d28137b4b1e78055f
Fangning <385556fc@student.eur.nl>
2017-04-04 23:05:22 +0200

    first commit

---
+++ A03.R
@@ -0,0 +1,28 @@
+# Dataset Links
+# https://bb-app02.ict.eur.nl/bbcswebdav/pid-179436-dt-content-rid-567048_1/courses/BMME012-16/whiskies.txt
+
+# Load packages 
+library(data.table)
+
+# Assignment
+
+# 6.1 Conceptual ####
+# [Q1] ####
+# [Q1](a) ####
+# [Q1](b) ####
+# [Q1](c) ####
+# [Q2] ####
+
+# 6.2 Applied ####
+# [Q3] ####
+# [Q3](a) ####
+# [Q3](b) ####
+# [Q4] ####
+# Data source 
+# https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data
+# Data Documentation
+# https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.names
+dt.wine <- fread("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header = FALSE)
+
+# [Q4](a) ####
+# [Q4](b) ####
